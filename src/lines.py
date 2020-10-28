d39170430bcb2f6cd3a1ac6bf0dfbf0abff3eb55
donpuz <lypuzon@ctemc.org>
2020-10-28 16:41:48 -0400

    hysteresis thresholding

--- src/lines.py
+++ src/lines.py
@@ -1,5 +1,11 @@
+import numpy as np
+from imageio import imread, imsave
+from skimage import data, filters
+from matplotlib import pyplot as plt
+import cv2
 def lines(img):
     gray = cv2.bilateralFilter(img, 15, 80, 80)
+    gray = cv2.gaussianFilter()
     #sigma 0.2 low 0.1 high 0.3
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     sobelx = cv2.Sobel(gray,cv2.CV_64F,1,0,ksize=5)
@@ -8,8 +14,10 @@ def lines(img):
     #energyImage = np.sqrt(energyImage)
     energyImage = abs(sobelx) + abs(sobely)
     energyImage = energyImage / np.amax(energyImage)
-    idx = energyImage[:,:] < 0.2
-    img[idx] = [0, 0, 0]
+    idx = energyImage > 0.35
+    hyst = filters.apply_hysteresis_threshold(energyImage, 0.1, 0.35)
+
+    img[hyst] = [0, 0, 0]
     
     return img
 
